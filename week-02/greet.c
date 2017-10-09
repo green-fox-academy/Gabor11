d4bcc89746e5b537fb3a13507f2eeeda10fa8d01
Gabor11 <gabor@valami.com>
2017-10-09 14:42:01 +0200

    finish greet

---
+++ week-02/greet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "greet.h"
+
+// define a variable and assign the value `Greenfox` to it in a .h file
+// create a function called `greet` that greets it's input parameter
+// define more names to greet, call the function multiple times
+
+int main()
+{
+
+    greet(greenfox);
+    greet("Gabor");
+    greet("Somebody");
+
+    return 0;
+}
