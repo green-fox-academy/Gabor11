3eb79f91c80cdd641988411fe239ef4702766d57
Gabor11 <gabor@valami.com>
2017-10-11 16:01:51 +0200

    memoization_variables_from_monday__and_sieve_of_eratosthenes2

--- week-02/day-1/variables.c
+++ week-02/day-1/variables.c
@@ -19,33 +19,33 @@ int main()
 {
     int input_ = 0;
     printf("Pls give me an integer:\n");
-    scanf("%d\n", &input_);
+    scanf("%d", &input_);
 
-    printf("%d\n",input_);
+    printf("And the result: %d\n", modulo_5(devide_integer_by_4(substract_2(multiply_by_2(add_2(input_))))));
 
-    // printf("And the result: %d\n", modulo_5(devide_integer_by_4(substract_2(multiply_by_2(add_2(input_))))));
+    printf("Operation counter: %d\n", op_count);
 
     return 0;
 }
 
 int add_2(int input){
-    return input + 2;
     op_count++;
+    return input + 2;
     }
 int multiply_by_2(int input){
-    return input *2;
     op_count++;
+    return input *2;
     }
 int substract_2(int input){
-    return input - 2;
     op_count++;
+    return input - 2;
     }
 int devide_integer_by_4(int input){
-    return input / 4;
     op_count++;
+    return input / 4;
     }
 int modulo_5(int input){
-    return input % 5;
     op_count++;
+    return input % 5;
     }
 
