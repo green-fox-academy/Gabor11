fdf4513661b36fccb8a46b837dad7ad8ef6ebbd9
Gabor11 <gabor@valami.com>
2017-10-12 14:30:12 +0200

    anagram_ready

--- week-02/day-4/anagram.c
+++ week-02/day-4/anagram.c
@@ -9,7 +9,7 @@ int anagram();
 int main()
 {
     char* input1 = "valami";
-    char* input2 = "imaval";
+    char* input2 = "lvamai";
 
     printf("%s\n",input1);
     printf("%s\n",input2);
@@ -27,50 +27,45 @@ int anagram(char* input1, char* input2)
 {
     int size = 0;
     int i = 0;
+    int j = 0;
     int check = 0;
-
-    // checking the sizes of the two inputs and setting the 'size' variable if they are equal
-    if (strlen(input1) != strlen(input2)){
-        printf("different size\n");
-        return false;
-    } else {
-        size = sizeof(input1)/sizeof(char);
-    }
-
-    //filling up the 'checker' array with zeros
+    //filling up the 'checker' array with zeros, the program will only able to deal with words under 100
     int8_t checker[100] = {0};
 
-    for (int i = 0; i < size; i++){
-        printf("%d\n", checker[i]);
+    // checking the sizes of the two inputs and setting the 'size' variable if they are equal
+    if (strlen(input1) != strlen(input2)) {
+        printf("different size.\n");
+        return false;
+    } else {
+        size = strlen(input1);
     }
-    printf("-----------------\n");
-    printf("size is: %d\n", size);
-
-    while (i < size){
-        for (int j = 0; j < size; j++){
-            if (checker[j] == true){
+    while (i < size) {
+        j = 0;
+        while (j < size) {
+            // this check is to jump over the elements that have been already found
+            while (checker[j] == true) {
                 j++;
+                // if we reach the size that means we have too many of the character stored actually in 'input1[i]'
                 if (j == size){
                     i = size;
                     break;
                 }
-            } else if (input1[i] == input2[j]){
-                checker[j] = true;
             }
-            printf("j: %d\n", j);
+            if (j < size && input1[i] == input2[j]) {
+                checker[j] = true;
+                break;
+            }
+            j++;
         }
-        printf("i: %d\n", i);
         i++;
     }
-
     //checking whether all the elements of 'checker' are set to true..
-    for (int k = 0; k < size; k++){
+    for (int k = 0; k < size; k++) {
         check += checker[k];
     }
-
     if (check == size)
         return true;
     else
-        printf("i is: %d",i);
+        printf("not all characters are identical.\n");
         return false;
 }
