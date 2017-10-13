2b652977c7334880c9fd8471e60b826052b25983
Gabor11 <gabor@valami.com>
2017-10-13 13:27:09 +0200

    finished palindrome search

--- week-02/day-4/search_palindrome_v3.c
+++ week-02/day-4/search_palindrome_v3.c
@@ -13,16 +13,14 @@ int main()
     char buff[256] = {'/0'};
 
     //inserting data
-    //printf("Pls insert a line of text: \n");
-    //gets(string);
+    printf("Pls insert a line of text: \n");
+    gets(string);
 
     //printf("%d",compare(string, 0, 3, 0));
-    ins_to_tar(target, "habakukk", 0, 8);
+    // ins_to_tar(target, "habakukk", 0, 8);
 
-
-
+    printf("%d palindromes found.\n",palindrom_search(string, target));
     printf("%s\n", target);
-    // palindrom_search(string, target);
 
     return 0;
 }
@@ -31,6 +29,7 @@ int8_t palindrom_search(char* string, char* tar)
 {
     int8_t len = strlen(string);
     int8_t count = 0;
+    int8_t hit_count = 0;
 
     for (int8_t h = 3; h <= len; h++) {
         for (int8_t i = 0; i < len; i++) {
@@ -38,13 +37,20 @@ int8_t palindrom_search(char* string, char* tar)
                 if (compare(string, i, h, j))
                     count++;
             }
-            if (count == h / 2)
-                ins_to_tar(target, string, i, h);
+            if (count == h / 2) {
+                ins_to_tar(tar, string, i, h);
+                hit_count++;
+            }
             count = 0;
         }
     }
+    tar[strlen(tar)-1] = '\0';
+    tar[strlen(tar)-1] = ']';
 
+    return hit_count;
 }
+
+
 
 int8_t compare(char* string, int8_t i, int8_t h, int8_t j)
 {
