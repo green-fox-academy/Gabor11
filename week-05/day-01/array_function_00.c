f13c3a45c1591c38df7421bbb93ba832af2cb5b3
Gabor11 <gabor@valami.com>
2017-10-30 10:14:16 +0100

    array_function

---
+++ week-05/day-01/array_function_00.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+
+/*
+ * Create a function that takes an array of integers as an argument (a pointer) and it's length as an unsigned integer.
+ * The function should sort the elements of the array in ascending order.
+ *
+ * Print it out before and after to be able to check whether it works as supposed.
+ */
+int main()
+{
+    int arr[] = {12, 11, 9, 23, 10, 8, 4, 50, 43, 32};
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    sorter(arr, 10);
+
+    printf("\n\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
+
+void sorter(int *array, int length)
+{
+    int aux = 0;
+    for (int i = 0; i < length; ++i) {
+        for (int j = i + 1; j < length; ++j) {
+            if (array[i] > array[j]) {
+                aux = array[i];
+                array[i] = array[j];
+                array[j] = aux;
+            }
+        }
+    }
+}
