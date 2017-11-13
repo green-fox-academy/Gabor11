439100dafb58a788231af10c4ae1a7868a51c38d
Gabor11 <gabor@valami.com>
2017-11-13 16:10:54 +0100

    dynamic memory allocation - 00

---
+++ week-06/DMA/00.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// store your personal data in Variables
+// string name, int age, int slice_of_pizza_you_can_eat
+// allocate memory dynamically for these variables
+// create a sentense using these variables
+// free the allocated memory
+
+int main() {
+    string *name = new string;
+    int *age = new int;
+    int *slice_of_pizza_you_can_eat = new int;
+
+    *name = "Gabor";
+    *age = 27;
+    *slice_of_pizza_you_can_eat = 6;
+
+    cout << "I am " << *name << ", I am " << *age << " years old and I can eat " << *slice_of_pizza_you_can_eat << " slices of pizza" << endl;
+
+    return 0;
+}
