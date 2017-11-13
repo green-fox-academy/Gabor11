0201618ee70fbeac5e91450007c00b9680306380
Gabor11 <gabor@valami.com>
2017-11-13 12:54:06 +0100

    datatypes - 00

--- week-06/Datatypes/00.cpp
+++ week-06/Datatypes/00.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+using namespace std;
+
+// Write a program that prints a few details to the terminal window about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Do you like coffee? (as a boolen variable)
+//
+//  Example output:
+//  John Doe
+//  31
+//  Likes coffee: true
+
+int main()
+{
+    bool coffee_like = true;
+
+    cout << "Gabor Matejka" << endl;
+    cout << 27 << endl;
+    cout << "Likes coffee: ";
+
+    if (coffee_like) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
