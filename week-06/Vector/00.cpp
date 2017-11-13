dd884c799ea95ef54af490e692a96e601f6f2b49
Gabor11 <gabor@valami.com>
2017-11-13 14:20:50 +0100

    vector - 00

---
+++ week-06/Vector/00.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//create an integer vector with the size of 5 and print the 3rd element of it
+
+int main() {
+
+    vector <int> vect;
+
+    vect.push_back(21);
+    vect.push_back(3);
+    vect.push_back(24);
+    vect.push_back(13);
+    vect.push_back(22);
+
+    cout << vect[2] << endl;
+
+    return 0;
+}
