eec5e75b1e3edb1433d59d18cdbd18def4b958c4
Gabor11 <gabor@valami.com>
2017-11-14 16:24:00 +0100

    oop - creating header files - 01, 02, 03

--- week-06/day-02/02.cpp
+++ week-06/day-02/02.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 #include <string>
+#include "02.h"
 
 using namespace std;
 
-class Student {
-  public:
-    string name;
-    unsigned int age;
-    Student(string name, unsigned int age) {
+Student::Student(string name, unsigned int age)  {
       this->age = age;
       this->name = name;
     }
-};
 
 int main() {
   // Create a new instance of the Student class and set it's name to "John" and
--- week-06/day-02/03.cpp
+++ week-06/day-02/03.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <string>
+#include "03.h"
 
 using namespace std;
 
-class Student {
-  public:
-    string name;
-    unsigned int age;
-    Student(string name, unsigned int age) {
-      this->age = age;
-      this->name = name;
-    }
-    string greet()
-    {
-        return "Hello my name is " + name;
-    }
+Student::Student(string name, unsigned int age)
+{
+    this->age = age;
+    this->name = name;
+}
 
-};
+string Student::greet()
+{
+    return "Hello my name is " + name;
+}
 
 int main() {
   // Create a method on the Student class called "greet" that returns a string
